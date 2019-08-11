340ef8bf1df190721c1b1bf4a6f12217514dc43a
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-11 17:01:52 -0400

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,11 @@
+class Student
+
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end  # ends hello method
+
+  def raise_hand
+    puts "Pick me!"
+  end  # ends raise_hand method
+end  # ends Studen class
